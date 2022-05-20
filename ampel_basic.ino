11b59171d7d8e605e510dd43a66c71fb98795607
NadiaSchmitz <nadiiaschmitz@gmail.com>
2022-05-20 08:14:48 +0200

    Video

--- ampel_basic.ino
+++ ampel_basic.ino
@@ -1,19 +1,16 @@
 int ledred = 11;
 int ledyellow = 12;
 int ledgreen = 13;
-int button = 7;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(ledred,OUTPUT);
   pinMode(ledyellow,OUTPUT);
   pinMode(ledgreen,OUTPUT);
-  pinMode(button, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(digitalRead(button)==LOW) {
     digitalWrite(ledred,HIGH);
     delay(2000);
     digitalWrite(ledyellow,HIGH);
@@ -23,9 +20,4 @@ void loop() {
     digitalWrite(ledgreen,HIGH);
     delay(2000);
     digitalWrite(ledgreen,LOW);
-  } else if (digitalRead(button)==HIGH) {
-    digitalWrite(ledred,LOW);
-    digitalWrite(ledyellow,LOW);
-    digitalWrite(ledgreen,LOW);
-  }
 }
